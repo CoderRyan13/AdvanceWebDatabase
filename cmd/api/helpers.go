7f4cffb88b2933db0bda4ebe34e91d60ca0cdcf9
Ryan Armstrong <armstrongryan11@gmail.com>
2022-09-27 19:00:40 -0600

    updated with more error handlers

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,6 +5,8 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -42,5 +44,45 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.WriteHeader(status)
 	// Write the []byte slice containing the JSON response body
 	w.Write(js)
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
+	// Use http.MaxBytesReader() to limit the size of the request body to
+	// 1 MB 2^20
+	//maxBytes := 1_048_576
+	// Decode the request body into the target destination
+	err := json.NewDecoder(r.Body).Decode(dst)
+	// Check for a bad request
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		// Switch to check for the errors
+		switch {
+		// Check for syntax errors
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON(at character %d)", syntaxError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errors.New("body contains badly-formed JSON")
+		// Check for wrong types passed by the client
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		// Empty body
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+		// Pass non-nil pointer error
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+		// default
+		default:
+			return err
+		}
+	}
+
 	return nil
 }
