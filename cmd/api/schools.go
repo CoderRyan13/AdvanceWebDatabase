5deb7eb989ed3391c975899eef238a57484d64a3
Ryan Armstrong <armstrongryan11@gmail.com>
2022-10-09 12:05:28 -0600

    added validation code

--- cmd/api/schools.go
+++ cmd/api/schools.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"appletree.ryanarmstrong.net/internal/data"
+	"appletree.ryanarmstrong.net/internal/validator"
 )
 
 // createSchoolHandler for the "Post /v1/schools" endpoint
@@ -27,6 +28,26 @@ func (app *application) createSchoolHandler(w http.ResponseWriter, r *http.Reque
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new School struct
+	school := &data.School{
+		Name:    input.Name,
+		Level:   input.Level,
+		Contact: input.Contact,
+		Phone:   input.Phone,
+		Email:   input.Email,
+		Website: input.Website,
+		Address: input.Address,
+		Mode:    input.Mode,
+	}
+	// Initialize a new Validator instance
+	v := validator.New()
+
+	// Check the map to determine if there were any validation errors
+	if data.ValidateSchool(v, school); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	// Display the request
